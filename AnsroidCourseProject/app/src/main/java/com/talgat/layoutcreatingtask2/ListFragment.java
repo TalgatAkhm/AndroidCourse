75547437bd6468071448831fa87ab53adbabace4
tahm <tahm@1c.ru>
2019-12-02 23:44:32 +0300

    first realization was provided

---
+++ AnsroidCourseProject/app/src/main/java/com/talgat/layoutcreatingtask2/ListFragment.java
@@ -0,0 +1,82 @@
+package com.talgat.layoutcreatingtask2;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Objects;
+
+public class ListFragment extends Fragment implements NotesRvAdapter.Listener {
+    public static final String NAME = ListFragment.class.getName();
+
+    private FrameLayout currentFrameLayout;
+
+    public static Fragment create() {
+        return new ListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull final LayoutInflater inflater,
+            @Nullable final ViewGroup container,
+            @Nullable final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        currentFrameLayout = new FrameLayout(Objects.requireNonNull(getActivity()));
+
+        final RecyclerView recyclerView = view.findViewById(R.id.notesRecyclerView);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            recyclerView.setLayoutManager(new GridLayoutManager(currentFrameLayout.getContext(), 2));
+        else
+            recyclerView.setLayoutManager(new LinearLayoutManager(currentFrameLayout.getContext()));
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.getRecycledViewPool().setMaxRecycledViews(0, 3);
+
+        final NotesRvAdapter adapter = new NotesRvAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.setNotes(NotesStorage.getNotes());
+        adapter.setListener(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Objects.requireNonNull(getActivity()).finish();
+        System.exit(0);
+    }
+
+    @Override
+    public void onNoteClick(long id) {
+        ((MainActivity) getActivity()).showDetailsFragment(id);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        final RecyclerView recyclerView = currentFrameLayout.findViewById(R.id.notesRecyclerView);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            recyclerView.setLayoutManager(new GridLayoutManager(currentFrameLayout.getContext(), 2));
+        else
+            recyclerView.setLayoutManager(new LinearLayoutManager(currentFrameLayout.getContext()));
+    }
+}
--- AnsroidCourseProject/app/src/main/java/com/talgat/layoutcreatingtask2/MainActivity.java
+++ AnsroidCourseProject/app/src/main/java/com/talgat/layoutcreatingtask2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.talgat.layoutcreatingtask2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -9,45 +10,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements NotesRvAdapter.Listener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        this.setTitle(R.string.main_activity_title);
 
-        final RecyclerView recyclerView = findViewById(R.id.notesRecyclerView);
-
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
-            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        else
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        recyclerView.setHasFixedSize(true);
-        recyclerView.getRecycledViewPool().setMaxRecycledViews(0, 3);
-
-        final NotesRvAdapter adapter = new NotesRvAdapter();
-        recyclerView.setAdapter(adapter);
-        adapter.setNotes(NotesStorage.getNotes());
-        adapter.setListener(this);
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.listContainer, ListFragment.create(), ListFragment.NAME)
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 
-    @Override
-    public void onNoteClick(long id) {
-        startActivity(NoteInfoActivity.getIntent(this, id));
-    }
+    public void showDetailsFragment(@NonNull final long id) {
+        if(getSupportFragmentManager().findFragmentByTag(DetailsFragment.NAME) != null){
+            getSupportFragmentManager().popBackStack();
+        }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        final RecyclerView recyclerView = findViewById(R.id.notesRecyclerView);
-
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
-            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        else
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.detailsContainer, DetailsFragment.create(id), DetailsFragment.NAME)
+                .addToBackStack(null)
+                .commit();
     }
 }
